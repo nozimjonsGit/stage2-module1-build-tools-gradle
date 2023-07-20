2da0b3d0c794c59065ca847a520fdbc7fbf5f926
Nozimjon Nabiev <73819566+NozimjonNabiev@users.noreply.github.com>
2023-07-21 00:58:30 +0500

    Done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,11 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        for (String str : args) {
+            if (!StringUtils.isPositiveNumber(str)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,6 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        return StringUtils.isNumeric(str) && Double.parseDouble(str) > 0;
     }
 }
