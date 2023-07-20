152c7a1431b31513d61b060f2b4e970882d8e24e
Nozimjon Nabiev <73819566+NozimjonNabiev@users.noreply.github.com>
2023-07-21 01:03:33 +0500

    Done

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,8 +1,10 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        return StringUtils.isNumeric(str) && Double.parseDouble(str) > 0;
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
